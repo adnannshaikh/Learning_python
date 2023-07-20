1b18b3b81f5a65c316a3713a77abb4a44da6d57e
adnannshaikh <101106625+adnannshaikh@users.noreply.github.com>
2023-07-20 20:50:59 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,20 @@
+apple = True
+carrot = True
+
+if apple:
+	print("Here have an apple ")
+elif carrot:
+	print("have a carrot")
+else:
+	print("stay hungry")
+
+
+#ternary opearator(Conditional expression)
+
+
+#condition_if_true if condition else condition_if_false
+#print(condition_if_true)
+
+
+# keywords
+# > < >= <= == != and or not is
